9f1d7d72d60083efecfef880cad00a806866bd25
sidneylino <sidneymartinslino@gmail.com>
2024-09-10 13:22:02 -0300

    fundamentos 002

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-//hello world
-const mensagem = "ola eu"
-{
-    const mensagem = "ola mundo"
+// arrays, objetos:
+
+//array
+let metas = ["mayk", "alo"]
+console.log(metas[1] + ", " + metas[0])
+
+//objeto
+let meta = {
+    value: "ler um livro por mes",
+    check: false,
+    log: (info) => {
+        console.log(info)
+    }
 }
-console.log(mensagem) //aparece ola eu e dps ola mundo
+console.log(meta.value)
+meta.log(meta.value)
+
+//arrow function 
+//const criarMeta = () => {}
+
+//function criarMeta() {}
