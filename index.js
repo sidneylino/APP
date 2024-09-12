1b40a3055ec9ecb4a52c0b322d981a3cd9b274ae
sidneylino <sidneymartinslino@gmail.com>
2024-09-12 20:54:46 -0300

    fundamentos 005

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const start = () => {
+const { select } = require('@inquirer/prompts')
+
+const start = async() => {
 
     while(true){
-        let opcao = "sair"
+
+        const opcao = await select({
+            message: "menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
         switch (opcao) {
             case "cadastrar":
                 console.log('vamos cadastrar') // faz loop
