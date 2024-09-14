9c81a62b64f859e9a72153517b183b0667b46cbd
sidneylino <sidneymartinslino@gmail.com>
2024-09-14 15:10:05 -0300

    resolver problema marcadas/desmarcadas

--- index.js
+++ index.js
@@ -26,14 +26,15 @@ const listarMetas = async () => {
         choices: [...metas],
         instructions: false,
     })
+    
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
     if(respostas.length == 0) {
         console.log("nenhuma meta selecionada")
         return
     }
-
-    metas.forEach((m) => {
-        m.checked = false
-    })
 
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
